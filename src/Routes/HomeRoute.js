544287de65fe2aa0ff6d9e8689bd0cc2e51cfd9a
Sophie <sophie.lindstrom90@gmail.com>
2023-01-30 12:36:55 +0100

    search box on home page

--- src/Routes/HomeRoute.js
+++ src/Routes/HomeRoute.js
@@ -1,150 +1,30 @@
-import React from 'react';
+import TextField from '@mui/material/TextField';
+import List from 'components/List';
+import React, { useState } from 'react';
 
 function HomeRoute() {
+  const [inputText, setInputText] = useState('');
+
+  const inputHandler = (event) => {
+    //convert input text to lower case
+    var lowerCase = event.target.value.toLowerCase();
+    setInputText(lowerCase);
+  };
+
   return (
-    <div>
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
-      Home
-      <br />
+    <div className="page">
+      <h1>PartyTime Destinations</h1>
+      <div className="search">
+        <TextField
+          id="outlined-basic"
+          onChange={inputHandler}
+          variant="filled"
+          color="primary"
+          fullWidth
+          label="Where to?"
+        />
+      </div>
+      <List input={inputText} />
     </div>
   );
 }
---
+++ src/components/List.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+
+const data = [
+  {
+    id: 1,
+    text: 'Stockholm',
+  },
+  {
+    id: 2,
+    text: 'Paris',
+  },
+  {
+    id: 3,
+    text: 'New York',
+  },
+  {
+    id: 4,
+    text: 'Los Angeles ',
+  },
+  {
+    id: 5,
+    text: 'Tokyo',
+  },
+  {
+    id: 6,
+    text: 'Bangkok',
+  },
+  {
+    id: 7,
+    text: 'Jakarta',
+  },
+  {
+    id: 8,
+    text: 'Göteborg',
+  },
+  {
+    id: 9,
+    text: 'Köpenhamn',
+  },
+  {
+    id: 10,
+    text: 'Berlin',
+  },
+];
+
+function List(props) {
+  //create a new array by filtering the original array
+  const filteredData = data.filter((el) => {
+    //if no input the return the original
+    if (props.input === '') {
+      return el;
+    }
+    //return the item which contains the user input
+    else {
+      return el.text.toLowerCase().includes(props.input);
+    }
+  });
+  return (
+    <ul>
+      {filteredData.map((item) => (
+        <li key={item.id}>{item.text}</li>
+      ))}
+    </ul>
+  );
+}
+
+export default List;
