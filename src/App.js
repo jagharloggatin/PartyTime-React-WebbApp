e63476b3a56edd6529650c3f4d500de28d4145f0
Sophie <sophie.lindstrom90@gmail.com>
2023-02-13 00:37:43 +0100

    fetch reviews on profile for user

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import UserList from 'routes/apiTest';
-import ProfileRewievsTab from 'routes/ProfieRewievsTab';
 import ProfileActivitiesTab from 'routes/ProfileActivitiesTab';
 import { default as ProfileCommentsTab, default as ProfileHomeTab } from 'routes/ProfileCommentsTab';
 import ProfileFavoritesTab from 'routes/ProfileFavoritesTab';
+import ProfileReviewsTab from 'routes/ProfileReviewsTab';
 import SettingsRoute from 'routes/SettingsRoute';
 import './App.scss';
 import AppNewActivityForm from './components/activities/AppNewActivityForm';
@@ -19,25 +19,22 @@ import SelectedActivityRoute from './routes/SelectedActivityRoute';
 import { default as SigninRoute, default as SignupRoute } from './routes/SignupRoute';
 
 function App() {
-
-  
   function initMap() {
-    const map = new window.google.maps.Map(document.getElementById("map"), {
+    const map = new window.google.maps.Map(document.getElementById('map'), {
       center: { lat: -34.397, lng: 150.644 },
       zoom: 8,
-    })
+    });
   }
-  
 
   useEffect(() => {
     window.initMap = initMap;
-  })
+  });
 
   return (
     <div className="wrapper">
       <AppHeader></AppHeader>
       <Routes>
-        <Route path="/" element={<HomeRoute/>} />
+        <Route path="/" element={<HomeRoute />} />
         <Route path="/api" element={<UserList />} />
         <Route path="/map" element={<MapRoute />} />
         <Route path="/profile" element={<ProfileRoute />}>
@@ -45,7 +42,7 @@ function App() {
           <Route path="/profile/activities" element={<ProfileActivitiesTab />} />
           <Route path="/profile/favorites" element={<ProfileFavoritesTab />} />
           <Route path="/profile/comments" element={<ProfileCommentsTab />} />
-          <Route path="/profile/review" element={<ProfileRewievsTab />} />
+          <Route path="/profile/review" element={<ProfileReviewsTab />} />
         </Route>
         <Route path="/login" element={<LoginRoute />} />
         <Route path="/settings" element={<SettingsRoute />} />
