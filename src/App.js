88499742d1b5868de0d10e6c76e5bb354c99cea7
Sophie <sophie.lindstrom90@gmail.com>
2023-02-15 14:45:21 +0100

    added path to profile/created

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App() {
         <Route path="/profile" element={<ProfileRoute />}>
           <Route index element={<ProfileFavoritesTab />} />
           <Route path="/profile/reviews" element={<ProfileReviewsTab />} />
-          <Route index element={<ProfileActivitiesTab />} />
+          <Route path="/profile/created" element={<ProfileActivitiesTab />} />
         </Route>
         <Route path="/login" element={<LoginRoute />} />
         <Route path="/settings" element={<SettingsRoute />} />
